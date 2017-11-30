59c2cb3c90c3fdac6126a4d47413b41101a6da11
Nazar Ner <n.ner@scalr.com>
2017-11-30 18:17:13 +0200

    Changes:4

--- cookbooks/revizor-chef/metadata.rb
+++ cookbooks/revizor-chef/metadata.rb
@@ -2,5 +2,13 @@ maintainer       "Scalr Inc."
 maintainer_email "stunko@scalr.com"
 name             "revizor-chef"
 license          "All rights reserved"
-description      "Cookbook for verifying chef-solo in Scalr"
+description      "Cookbook for verifying chef-solo in Scalr ************************"
 version          "0.1.1"
+
+
+
+
+
+
+
+
