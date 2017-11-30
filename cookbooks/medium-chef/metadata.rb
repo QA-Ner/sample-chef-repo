1450df941b3fd68f6ec029c802688557d287386c
Nazar Ner <n.ner@scalr.com>
2017-11-30 17:16:15 +0200

    Changes:

---
+++ cookbooks/medium-chef/metadata.rb
@@ -0,0 +1,6 @@
+name             'medium-chef'
+maintainer       'Scalr Inc.'
+maintainer_email 'n.kalmikov@scalr.com'
+license          'All rights reserved'
+description      'Cookbook for medium chef tests'
+version          '0.1.0'
